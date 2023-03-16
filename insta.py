146764d67205b579cdd92f1cf523e250c182a79c
Vicente Lanari <vicentelanari@gmail.com>
2023-03-16 21:35:16 +0100

    non precise scraper

--- insta.py
+++ insta.py
@@ -3,7 +3,7 @@ from app import *
 def main():
     insta_url = input("digite o fim do url do usuário(por exemplo em 'instagram.com/exemplo' seria exemplo): ")
     num = get_insta_followers(insta_url)
-    return num
+    print(num)
 
 if __name__ == "__main__":
     main()
